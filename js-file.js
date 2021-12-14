db61427736ab2892771026d13c9d5faab994a492
Cody <whearcody@gmail.com>
2021-12-14 17:46:12 +0000

    Added the form that asks users for the width/ number of squares and got that to work, also added a clear form button. Default width is 4.

--- js-file.js
+++ js-file.js
@@ -1,21 +1,34 @@
 document.body.onload = createDivs;
-const input = 15;
-let width = 600 / input;
-console.log(width);
+
+let input = 4;
 const container = document.getElementById("container");
 
 function createDivs () {
-    for (let i = 0; i < input * input; i++) {
+    clearGrid();
+    let width = 600 / input;
+    for (let i = 0; i < input **2; i++) {
         const newDiv = document.createElement("div");
         newDiv.setAttribute("class", "newDiv");
         newDiv.style.width = width + "px";
         newDiv.style.height = width + "px";
         container.appendChild(newDiv);
-        console.log("creating thing");
         newDiv.addEventListener('pointerover', (e) => {
-            e.target.style.background = 'cyan';
+            e.target.style.background = 'red';
         });
     }
 }
-
+function getInput() {
+    if (document.getElementById("mySelect").value) {
+        input = document.getElementById("mySelect").value;
+    } else {
+        input = 4;
+    }
+    createDivs();
+}
+function clearGrid() {
+    let element = document.getElementById("container");
+        while (element.firstChild) {
+        element.removeChild(element.firstChild);
+}
+}
 
